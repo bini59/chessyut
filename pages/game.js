b3d752b244e79784a545878c0277abb523c3cadd
myuni <myuni0234@gmail.com>
2022-03-10 20:21:38 +0900

    Feat : Change chess board
    체스판을 map을 이용해서 다시 만들었다.
    
    시작점과 도착점을 표시했고,
    기물이 이동할 수 있는 위치는 따로 표시를 해줬다.
    
    또, 현재 기물의 위치를 표시하는 변수를 만들었다.
    기물을 체스판 위에 나타내느것 까지 완료했다.

--- pages/game.js
+++ pages/game.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Image from "next/image";
 
 import Timer from "./component/timer";
 
@@ -40,23 +41,24 @@ const style = `
     .timer{
         position: absolute;
         top: 5px;
-        left: 48vw;
+        left: 50vw;
         font-size: 3rem;
     }
 
     .board{
-        border : 1px solid #000;
-        width : 10vw;
-        height: 10vh;
+        margin-top : 10vh;
+        width : 30vw;
+        height: 30vw;
         margin-left : auto;
         margin-right: auto;
     }
     
-    .chess-board { border-spacing: 0; border-collapse: collapse; }
-    .chess-board th { padding: .5em; }
-    .chess-board td { border: 1px solid; width: 2em; height: 2em; }
-    .chess-board .light { background: #eee; }
-    .chess-board .dark { background: #000; }
+    .chess-board { border: 1px solid; border-spacing: 0; border-collapse: collapse; }
+    .chess-board td { width: 3.75vw; height: 3.75vw; position:relative; text-align: center;}
+    .chess-board .light { background: #f0d9b5; }
+    .chess-board .dark { background: #b58863; }    
+    .chess-board .use-normal{ border-radius : 20px; border: 3px solid #8f2700; }
+    .chess-board .use-knight{ border: 3px solid #8f2700; }
 `
 
 const User = ()=>{
@@ -99,117 +101,80 @@ const User = ()=>{
     )
 }
 
-const Board = ()=>{
+// (!(p%2)||(q%2))&&(!(q%2)||(p%2))
+
+const Board = (props)=>{
+    let B = [
+        [0, 4, 4, 4, 4, 4, 4, 0],
+        [4, 1, 1, 1, 1, 1, 1, 4],
+        [4, 1, 1, 0, 0, 0, 1, 4],
+        [4, 1, 0, 1, 0, 0, 1, 4],
+        [4, 1, 0, 0, 1, 0, 1, 4],
+        [4, 1, 0, 0, 0, 1, 1, 4],
+        [4, 2, 0, 0, 0, 0, 3, 4],
+    ]
+
+    const pics = props.pics;
+
+    let Board = B.map((R, ri)=>{
+        let row = R.map((I, iI)=>{
+            return(
+                <td key={ri*10+iI} className={((!(ri%2)||(iI%2))&&(!(iI%2)||(ri%2)) ? "light": "dark")+(I>0 ? ((I < 4) ? " use-normal": " use-knight")  : " not-use")} >
+                    {(pics[ri][iI] != 0) ? <Image src={"/pieces/"+(pics[ri][iI])+".png"} layout="fill" /> : "" }
+                    {(I == 3 ? "End" : (I == 2 ? "start" : ""))}
+                </td>
+            )
+        })
+        return(
+            <tr key={ri*100}>{row}</tr>
+        )
+    })
+    
+
+
     return (
         <section className="board">
-            <table class="chess-board">
-            <tbody>
-                <tr>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                </tr>
-                <tr>
-                
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                </tr>
-                <tr>
-                
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                </tr>
-                <tr>
-                
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                </tr>
-                <tr>
-                
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                </tr>
-                <tr>
-                
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                </tr>
-                <tr>
-                
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                </tr>
-                <tr>
-                
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                </tr>
-            </tbody>
-        </table>
+            <table className="chess-board">
+                <tbody>{Board}</tbody>
+            </table>
         </section>
     );
 }
 
+
 const Game = ()=>{
     const [time, setTime] = useState(0);
 
+    const [peices, setPics] = useState([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, "bB", 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+    
 
     return (
         <>
             <Timer time={time} settime={(t)=>{setTime(t)}}/>
             <User/>
 
-            <Board />
+            <Board pics={peices}/>
             {/* test timer button */}
             {/* <button onClick={()=>{setTime(10)}}>tt</button>
             <button onClick={()=>{setTime(5)}}>tt</button> */}
+
+            {/* <button onClick={()=>{setPics([
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, "bB", 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ])}}>12</button> */}
 
             <style jsx>{style}</style>
         </>
